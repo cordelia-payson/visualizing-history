5d92bb387d477283992a824ec623209e5daed466
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-10 13:56:18 -0500

    basic setup for MUI masonry

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Photo() {};
+function Photo(props) {
+  const { image } = props;
+
+  return (
+    <div>{image}</div>
+  );
+}
 
 export default Photo;
--- client/src/components/PhotoFeed.jsx
+++ client/src/components/PhotoFeed.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import Masonry from '@mui/lab/Masonry';
 import Photo from './Photo.jsx';
 
-function PhotoFeed() {};
+const FeedContainer = styled.div``;
+
+function PhotoFeed() {
+  const images = [1, 2, 3, 4, 5];
+  return (
+    <FeedContainer>
+      <Masonry columns={4} spacing={2}>
+        {images.map((image) => (
+          <Photo image={image} />
+        ))}
+      </Masonry>
+    </FeedContainer>
+  );
+}
 
 export default PhotoFeed;
